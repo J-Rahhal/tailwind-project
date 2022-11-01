0f1944f83cc3f631897f375a3fa6090a868dc98b
dev-acc-practice <development-practice@proton.me>
2022-11-01 10:07:38 +0200

    fixed hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       xl: '1440px'
     },
     extend: {
-      color: {
+      colors: {
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
@@ -18,7 +18,7 @@ module.exports = {
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0, 98%)',
-      }
+      },
     },
   },
   plugins: [],
